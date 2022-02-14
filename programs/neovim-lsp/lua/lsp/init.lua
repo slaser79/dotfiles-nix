d082512f53f91bbf185566e3ed0ee6384395e460
Shingi Mureriwa <shingi79@hotmail.com>
2022-02-14 09:31:12 +0100

    Added typescript language server

--- programs/neovim-lsp/lua/lsp/init.lua
+++ programs/neovim-lsp/lua/lsp/init.lua
@@ -36,7 +36,7 @@ local shared_config = {
 }
 
 local lspconfig = require('lspconfig')
-local servers = { 'sumneko_lua', 'rnix', 'ccls', 'texlab', 'bashls', 'pyright', 'sqls' , 'hls'}
+local servers = { 'sumneko_lua', 'rnix', 'ccls', 'texlab', 'bashls', 'pyright', 'sqls' , 'hls','tsserver'}
 
 -- Apply server-specific config from lsp dir
 for _, server in ipairs(servers) do
