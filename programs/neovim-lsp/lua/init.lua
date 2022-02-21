0a42d6f7a534135010ba30154780afe454c3f9f9
Shingi Mureriwa <shingi79@hotmail.com>
2022-02-21 06:52:05 +0100

    Highlighting yanked text

--- programs/neovim-lsp/lua/init.lua
+++ programs/neovim-lsp/lua/init.lua
@@ -31,7 +31,15 @@ require('nvim-treesitter.configs').setup({
   playground = { enable = true },
 })
 
--- indent blankline showing funning characters disabled for now 
+--Highlight yanked test
+vim.cmd [[
+    augroup highlight_yank
+    autocmd! 
+    au TextYankPost * silent!  lua vim.highlight.on_yank({higroup="Visual",timeout=200})
+    augroup END
+]]
+
+-- indent blankline showing funning characters disabled for now
 --[[require('indent_blankline').setup({
   show_trailing_blankline_indent = false,
   use_treesitter = true,
